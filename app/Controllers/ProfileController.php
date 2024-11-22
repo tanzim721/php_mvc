dadabdee46582d1662f1af4aac1ced4c72247de6
tanzim721 <53827851+tanzim721@users.noreply.github.com>
2024-11-22 23:55:33 +0600

    structure create

---
+++ app/Controllers/ProfileController.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace App\Controllers;
+
+class ProfileController extends Controller
+{
+    public function index()
+    {
+        echo "Profile";
+    }
+
+    public function edit()
+    {
+        echo "Edit Profile";
+    }
+}
+
+?>
