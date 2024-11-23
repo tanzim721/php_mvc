20fcda44ab315fb7def370e1a963f9dfc95a244e
tanzim721 <53827851+tanzim721@users.noreply.github.com>
2024-11-23 22:15:42 +0600

    controller

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php 
 
+use App\Controllers\ProfileController;
+
     require_once __DIR__ . '/../vendor/autoload.php';
 
     $routes = require_once __DIR__ . '/../routes/web.php';
@@ -7,6 +9,7 @@
     // print_r($routes); 
     
     $request = $_SERVER['REQUEST_URI'];
+    // print_r($request);
     
     $request = explode("?", $request)[0];
 
@@ -14,6 +17,9 @@
         // print_r($routes[$request]);
         $controller = $routes[$request][0] ?? null;
         $action = $routes[$request][1] ?? null;
+
+        // echo $controller;
+        // echo $action;
         
         if ($controller && $action) {
             $newController = new $controller();
@@ -23,5 +29,8 @@
     else {
         throw new Exception("404 Not Found");
     }   
+
+
+    echo ProfileController::class;
 
 ?>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php 
+
+use App\Controllers\ProfileController;
+
     return [
-        "/users/profile" => ["App\Controllers\ProfileController", "index"],
-        "/users/profile/edit" => ["App\Controllers\ProfileController", "edit"],
+        "/users/profile" => [ProfileController::class, "index"],
+        "/users/profile/edit" => [ProfileController::class, "edit"],
     ]
 
 ?>
